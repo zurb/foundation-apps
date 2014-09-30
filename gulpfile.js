62fd4d22b7e04420e45a41f55a2ee26984e96981
Antonin Januska <antonin@antjanus.com>
2014-09-30 13:56:05 -0500

    Updated packages and gulpfile to take over for the Grunt config

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,18 @@
 var gulp         = require('gulp'),
-    rimraf       = require('rimraf'),
+    rimraf       = require('gulp-rimraf'),
     server       = require('gulp-develop-server'),
     frontMatter  = require('gulp-front-matter'),
     path         = require('path')
     through      = require('through2')
     fs           = require('fs')
     autoprefixer = require('gulp-autoprefixer'),
-    sass         = require('gulp-ruby-sass');
+    sass         = require('gulp-ruby-sass'),
+    uglify       = require('gulp-uglify'),
+    concat       = require('gulp-concat');
 
 //prefix
 gulp.task('prefix', ['sass'], function() {
-  return gulp.src('./build/assets/**/*.css')
+  return gulp.src('./build/assets/css/*.css')
     .pipe(autoprefixer({
       browsers: ['last 2 versions', 'ie 10']
     }))
@@ -20,7 +22,9 @@ gulp.task('prefix', ['sass'], function() {
 
 // clean folders
 gulp.task('clean', function(cb) {
-  rimraf(['./build', './dist'], cb);
+  return gulp.src(['./dist', './build'])
+    .pipe(rimraf())
+  ;
 });
 
 //copy files
@@ -29,12 +33,40 @@ gulp.task('copy', ['clean'], function() {
     .pipe(gulp.dest('build'));
 });
 
+gulp.task('copy2', ['clean'], function() {
+  return gulp.src(['src/assets/**.*', '!**/*.js', '!**/*.scss'], { base: './src/'})
+    .pipe(gulp.dest('dist/assets'))
+  ;
+
+});
+
 //ruby sass
 gulp.task('sass', ['clean', 'copy'], function() {
   return gulp.src('scss/app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('dist/assets/css/src/app.css'))
+    .pipe(sass({ loadPath: ['scss', 'scss/foundation'] }))
+    .pipe(concat('dist/assets/css/app.css'))
   ;
+});
+
+//uglify and concat
+gulp.task('uglify', ['copy', 'clean'], function() {
+  var libs = ['bower_components/jqlit/jqlite.1.1.1.js', 'bower_components/fastlickc/lib/fastclick.js', 'bower_components/notify.js/notify.js', 'bower_components/tether/tether.js', 'js/foundation.js', 'js/foundation.modal.js', 'js/foundation.notification.js', 'js/foundation.offcanvas.js', 'js/foundation.popup.js', 'js/app.js'];
+
+  return gulp.src(libs)
+    .pipe(uglify())
+    .pipe(concat('dist/assets/js/all.js'))
+  ;
+
+});
+
+gulp.task('uglify-angular', ['copy', 'clean'], function() {
+  var libs = ['bower_components/angular-animate/angular-animate.js', 'bower_components/angular-animate', 'client/assets/js/*.js'];
+
+  return gulp.src(libs)
+    .pipe(uglify())
+    .pipe(concat('build/assets/js/all.js'))
+  ;
+
 });
 
 gulp.task('front-matter', ['copy'], function() {
@@ -71,7 +103,7 @@ gulp.task('server:start', ['build'], function() {
   server.listen( { path: 'app.js' });
 });
 
-gulp.task('build', ['copy', 'front-matter', 'prefix'], function() {
+gulp.task('build', ['clean', 'copy', 'copy2', 'sass', 'prefix', 'uglify','uglify-angular', 'front-matter'], function() {
   console.log('Successfully built');
 });
 
