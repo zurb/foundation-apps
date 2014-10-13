3f16aeb98ddfacf9fd45d6259239b682c79c7cc6
Antonin Januska <antonin@antjanus.com>
2014-10-13 13:46:31 -0500

    On-page template sourcing

--- js/angular/directives/interchange.js
+++ js/angular/directives/interchange.js
@@ -15,7 +15,7 @@ angular.module('foundation.interchange')
     replace: true,
     template: '<div></div>',
     link: function(scope, element, attrs, ctrl, transclude) {
-      var childScope, current, scenarios;
+      var childScope, current, scenarios, innerTemplates;
 
       var named_queries = {
         'default' : 'only screen',
@@ -54,51 +54,74 @@ angular.module('foundation.interchange')
         return matches;
       };
 
-      var collectScenarios = function(parentElement) {
+      var collectInformation = function(parentElement) {
         scenarios = [];
+        innerTemplates = [];
         var elements = parentElement.children();
         var i = 0;
 
         angular.forEach(elements, function(el) {
           var elem = angular.element(el);
-          scenarios[i] = { media: elem.attr('media'), src: elem.attr('src') };
+
+          //save on-page templates
+          if (!elem.attr('src')) {
+            innerTemplates[i] = el;
+            scenarios[i] = { media: elem.attr('media'), templ: i };
+          } else {
+            scenarios[i] = { media: elem.attr('media'), src: elem.attr('src') };
+          }
+
           i++;
         });
 
       };
 
+      var checkScenario = function(scenario) {
+        return !current || (scenario.src !== current.src) || (scenario.templ && scenario.templ !== current.templ);
+      };
+
+
       //setup
       foundationApi.subscribe('resize', function(msg) {
         transclude(function(clone, newScope) {
-          if(!scope.scenarios) {
-            collectScenarios(clone);
+          if(!scope.scenarios || !scope.innerTemplates) {
+            collectInformation(clone);
           }
           var ruleMatches = matched();
           var scenario = scenarios[ruleMatches[0].ind];
 
-          if(current && scenario.src == current.src) {
-          } else {
+          if(checkScenario(scenario)) {
             var compiled;
-            var loader = templateLoader(scenario.src);
 
             if(childScope) {
               childScope.$destroy();
               childScope = null;
             }
 
-            loader.success(function(html) {
+            if(scenario.templ) {
+              //on page template logic
               childScope = newScope;
-              element.html(html);
-              //compiled = $compile(html)(childScope);
-            }).then(function(){
+              element.html(innerTemplates[scenario.templ]);
               $compile(element.contents())(childScope);
               current = scenario;
-            });
+            } else {
+              //dynamic partial
+              var loader = templateLoader(scenario.src);
+              loader.success(function(html) {
+                childScope = newScope;
+                element.html(html);
+                //compiled = $compile(html)(childScope);
+              }).then(function(){
+                $compile(element.contents())(childScope);
+                current = scenario;
+              });
+            }
           }
         });
 
       });
 
+      //init
       foundationApi.publish('resize', 'initial resize');
 
     }
