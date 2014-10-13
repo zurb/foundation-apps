686d1cee1462393a36062a4371e1b72ce3b7b33e
Antonin Januska <antonin@antjanus.com>
2014-10-13 16:02:43 -0500

    Complete tabs rewrite

--- js/angular/directives/tabs.js
+++ js/angular/directives/tabs.js
@@ -1,18 +1,89 @@
 angular.module('foundation.tabs', []);
 
 angular.module('foundation.tabs')
-  .directive('faTabContent', ['FoundationApi', function(foundationApi) {
+  .controller('FaTabsController', function FaTabsController() {
+    var controller = this;
+    var tabs = controller.tabs = [];
+
+    controller.addTab = function addTab(tabScope) {
+      tabs.push({ scope: tabScope, active: false });
+
+      if(tabs.length === 1) {
+        tabs[tabs.length - 1].active = true;
+      }
+    };
+});
+
+/* Foundation Tabs
+ * creates a tabs collection and displays header tabs
+ *
+ */
+angular.module('foundation.tabs')
+  .directive('faTabs', ['FoundationApi', function(foundationApi) {
   return {
-    restrict: 'A',
-    template: '<div ng-transclude></div>',
+    restrict: 'EA',
     transclude: 'true',
     replace: true,
+    templateUrl: '/partials/tabs.html',
+    controller: 'FaTabsController',
     compile: function(tElement, tAttr) {
-      //set ID
+      if (!tAttr.id) {
+        tAtt.$set('id', foundationApi.generateUuid);
+      }
       return {
         pre: function preLink(scope, element, attrs) {
         },
         post: function postLink(scope, element, attrs) {
+
+          //update tabs in case tab-content doesn't have them
+          var updateTabs = foundationApi.publish(id, tabs);
+
+          foundationApi.subscribe(id + '-get-tabs', function() {
+            updateTabs();
+          });
+        }
+      };
+    }
+  };
+}]);
+
+/* uses Tab collection to display tabs and activates them when needed */
+angular.module('foundation.tabs')
+  .directive('faTabContent', ['FoundationApi', function(foundationApi) {
+  return {
+    restrict: 'A',
+    transclude: 'true',
+    replace: true,
+    scope: {
+      tabs: '=',
+      target: '@'
+    },
+    templateUrl: 'partials/tab-content.html',
+    compile: function(tElement, tAttr) {
+      return {
+        post: function postLink(scope, element, attrs, ctrl) {
+          scope.tabs = scope.tabs || [];
+          var id = scope.target;
+
+
+          foundationApi.subscribe(id + '-tabs-active', function(activeTab) {
+            angular.forEach(scope.tabs, function(tab) {
+              if(activeTab === tab.id) {
+                tab.active = true;
+              } else {
+                tab.active = false;
+              }
+            });
+          });
+
+          //if tabs empty, request tabs
+          if(scope.tabs === []) {
+            foundationApi.subscribe(id + '-tabs', function(tabs) {
+              scope.tabs = tabs;
+            });
+
+            foundationApi.publish(id + '-get-tabs', '');
+          }
         }
       };
     }
@@ -21,105 +92,43 @@ angular.module('foundation.tabs')
 
 angular.module('foundation.tabs')
   .directive('faTab', ['FoundationApi', function(foundationApi) {
-  return {
-    restrict: 'A',
-    link: function(scope, element, attrs, controller, transclude) {
-      var currentState = '';
-      var id = attrs.id;
-
-      scope.hide = function() {
-        element.removeClass('is-active');
-        currentState = 'hide';
-        return;
-      };
-
-      scope.show = function() {
-        element.parent().children().removeClass('is-active');
-        element.addClass('is-active');
-        currentState = 'show';
-        //inform others to close
-
-        return;
-      };
-
-      element.on('click', function(e) {
-        foundationApi.publish(attrs.faHref, attrs.faHref);
-        e.preventDefault();
-      });
-
-      foundationApi.subscribe(attrs.id + '-tab', function(msg) {
-        if(msg == 'show' || msg == 'open') {
-          scope.show();
-        } else if (msg == 'close' || msg == 'hide') {
-          scope.hide();
+    return {
+      restrict: 'A',
+      transclude: 'true',
+      replace: false,
+      scope: {
+        title: '@'
+      },
+      compile: function(tElement, tAttr) {
+        if (!tAttr.id) {
+          tAtt.$set('id', foundationApi.generateUuid);
         }
 
-        return;
-      });
+        return {
+          post: function postLink(scope, element, attrs, controller, transclude) {
+            scope.transcludeFn = transclude;
+            scope.id = attrs.id;
+            controller.addTab(scope);
 
-      //init
-      var init = function() {
-        var siblingsEl = element.parent().children();
-        var firstEl = angular.element(siblingsEl[0]);
-
-        if(!firstEl.hasClass('is-active')) {
-          firstEl.addClass('is-active');
-          foundationApi.publish(attrs.id + '-control', 'show');
-        }
-
-      };
-
-      init();
-    }
-  };
+          }
+        };
+      }
+    };
 }]);
 
 angular.module('foundation.tabs')
-  .directive('faTabHref', ['FoundationApi', function(foundationApi) {
-  return {
-    restrict: 'A',
-    link: function(scope, element, attrs, controller, transclude) {
-      var currentState = '';
+  .directive('faTabIndividual', ['FoundationApi', function(foundationApi) {
+    return {
+      restrict: 'A',
+      transclude: 'true',
+      replace: false,
+      link: function postLink(scope, element, attrs, ctrl, transclude) {
+        var tab = scope.$eval(attrs.tab);
 
-      scope.hide = function() {
-        element.removeClass('is-active');
-        currentState = 'hide';
-        return;
-      };
+        tab.transcludeFn(tab.scope, function(tabContent) {
+          element.append(tabContent);
 
-      scope.show = function() {
-        element.parent().children().removeClass('is-active');
-        element.addClass('is-active');
-        currentState = 'show';
-        return;
-      };
-
-      element.on('click', function(e) {
-        foundationApi.publish(attrs.faTabHref + '-tab', 'show');
-        scope.show();
-        e.preventDefault();
-      });
-
-      foundationApi.subscribe(attrs.faHref + '-control', function(msg) {
-        if(msg == 'show' || msg == 'open') {
-          scope.show();
-        } else if (msg == 'close' || msg == 'hide') {
-          scope.hide();
-        }
-
-        return;
-      });
-
-      //init
-      var init = function() {
-        var siblingsEl = element.parent().children();
-        var firstEl = angular.element(siblingsEl[0]);
-        if(!firstEl.hasClass('is-active')) {
-          firstEl.triggerHandler('click');
-        }
-      };
-
-      init();
-    }
-  };
+        });
+      }
+    };
 }]);
