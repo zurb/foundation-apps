31f0609dc64c9e47b6e266975d1b5ac2bf7227ee
Antonin Januska <antonin@antjanus.com>
2014-10-27 14:40:30 -0500

    Register dynamic routes outside of config

--- js/angular/common/foundation.init.js
+++ js/angular/common/foundation.init.js
@@ -56,3 +56,66 @@ angular.module('foundation.init')
       }
     };
 });
+
+$FoundationStateProvider.$inject = ['$stateProvider'];
+function $FoundationStateProvider($stateProvider) {
+  var complexViews = {};
+
+  this.registerDynamicRoutes = function() {
+    angular.forEach(dynamicRoutes, function(page) {
+      if (page.hasComposed == true) {
+        if (!angular.isDefined(complexViews[page.parent])) {
+          complexViews[page.parent] = { children: {} };
+        }
+
+        complexViews[page.parent]['children'][page.name] = page;
+      } else if (page.composed == true) {
+        if(!angular.isDefined(complexViews[page.name])) {
+          complexViews[page.name] = { children: {} };
+        }
+
+        angular.extend(complexViews[page.name], page);
+      } else {
+        var state = {
+          url: page.url,
+          templateUrl: page.path,
+          parent: page.parent || '',
+          controller: page.controller || 'DefaultController',
+          data: { vars: page },
+        };
+
+        $stateProvider.state(page.name, state);
+      }
+    });
+
+    angular.forEach(complexViews, function(page) {
+        var state = {
+          url: page.url,
+          parent: page.parent || '',
+          data: { vars: page },
+          views: { '': {
+              templateUrl: page.path,
+              controller: page.controller || 'DefaultController',
+            }
+          }
+        };
+
+        angular.forEach(page.children, function(sub) {
+          state.views[sub.name + '@' + page.name] = {
+            templateUrl: sub.path,
+            controller: page.controller || 'DefaultController',
+            };
+        });
+
+        $stateProvider.state(page.name, state);
+    });
+  }
+
+  this.$get = function() {
+    console.log('test');
+    return {};
+  };
+}
+
+angular.module('foundation.init')
+  .provider('$FoundationState', $FoundationStateProvider);
