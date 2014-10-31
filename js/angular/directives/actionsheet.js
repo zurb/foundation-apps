09d22356cf00639dfcc79750cf56bb8307e313eb
Antonin Januska <antonin@antjanus.com>
2014-10-31 10:04:38 -0500

    More versatile actionsheet

--- js/angular/directives/actionsheet.js
+++ js/angular/directives/actionsheet.js
@@ -1,20 +1,51 @@
 angular.module('foundation.actionsheet', []);
 
 angular.module('foundation.actionsheet')
+  .controller('FaActionSheetController', ['$scope', 'FoundationApi', function($scope, foundationApi) {
+    var controller = this;
+    var content = controller.content = $scope.content;
+
+    controller.registerContent = function(scope) {
+      content = scope;
+      content.active = false;
+    };
+
+    controller.toggle = function() {
+      content.toggle();
+      content.$apply();
+    };
+
+}]);
+
+angular.module('foundation.actionsheet')
   .directive('faActionSheet', ['FoundationApi', function(foundationApi) {
   return {
     restrict: 'EA',
     transclude: true,
     replace: true,
     templateUrl: '/partials/actionsheet.html',
+    controller: 'FaActionSheetController',
+    link: function(scope, element, attrs, controller) {
+    }
+  };
+}]);
+
+angular.module('foundation.actionsheet')
+  .directive('faAsContent', ['FoundationApi', function(foundationApi) {
+  return {
+    restrict: 'EA',
+    transclude: true,
+    replace: true,
+    templateUrl: '/partials/actionsheet-content.html',
+    require: '^faActionSheet',
     scope: {
-      position: '@?',
-      buttonTitle: '@?'
+      position: '@?'
     },
+    controller: function() { },
     link: function(scope, element, attrs, controller) {
       scope.active = false;
+      controller.registerContent(scope);
 
-      //setup
       foundationApi.subscribe(attrs.id, function(msg) {
         if(msg == 'show' || msg == 'open') {
           scope.show();
@@ -43,6 +74,29 @@ angular.module('foundation.actionsheet')
         scope.active = !scope.active;
         return;
       };
+    },
+  };
+}]);
+
+angular.module('foundation.actionsheet')
+  .directive('faAsButton', ['FoundationApi', function(foundationApi) {
+  return {
+    restrict: 'EA',
+    transclude: true,
+    replace: true,
+    templateUrl: '/partials/actionsheet-button.html',
+    require: '^faActionSheet',
+    scope: {
+      title: '@?'
+    },
+    controller: function() { },
+    link: function(scope, element, attrs, controller) {
+
+      element.on('click', function(e) {
+        controller.toggle();
+        e.preventDefault();
+      });
+
     },
   };
 }]);
