c061e7683cce4069419132fbcc825f3ef1bfb52b
Antonin Januska <antonin@antjanus.com>
2014-11-03 11:18:29 -0600

    Foundation Init refactor and test

--- tests/unit/common/initSpec.js
+++ tests/unit/common/initSpec.js
@@ -9,11 +9,11 @@ describe('Foundation for Apps Init', function() {
     }));
 
     it('should modify global settings with media queries', inject(function(FoundationInit, FoundationApi) {
-      expect(FoundationApi.getSettings()).toEqual([]);
+      expect(FoundationApi.getSettings()).toEqual({});
 
       FoundationInit.init();
 
-      expect(FoundationApi.getSettings()).toEqual([]);
+      expect(FoundationApi.getSettings()).toEqual({ media_queries : { small : 'Times New Roman', medium : 'Times New Roman', large : 'Times New Roman', xlarge : 'Times New Roman', xxlarge : 'Times New Roman' } });
     }));
   });
 
