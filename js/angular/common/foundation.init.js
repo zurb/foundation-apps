712bad5c1607904450fb2a49c07aa8969877733e
Antonin Januska <antonin@antjanus.com>
2014-12-12 15:46:00 -0600

    Moved controller into foundation.init.state where it makes sense

--- js/angular/common/foundation.init.js
+++ js/angular/common/foundation.init.js
@@ -137,3 +137,22 @@ angular.module('foundation.init.state', ['ui.router'])
       return {};
     };
 }]);
+
+angular.module('foundation.init.state')
+  .controller('DefaultController', ['$scope', '$stateParams', '$state', function($scope, $stateParams, $state) {
+    var params = [];
+    angular.forEach($stateParams, function(value, key) {
+      params[key] = value;
+    });
+
+    $scope.params = params;
+    $scope.current = $state.current.name;
+
+    if($state.current.views) {
+      $scope.vars = $state.current.data.vars;
+      $scope.composed = $state.current.data.vars.children;
+    } else {
+      $scope.vars = $state.current.data.vars;
+    }
+  }
+]);
