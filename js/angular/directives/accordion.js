ff28c5a74796371cd7772990d62e86ed79ba6c58
Antonin Januska <antonin@antjanus.com>
2014-12-12 12:04:28 -0600

    Multi open, auto open, and collapsible fixes for Accordion based on #258

--- js/angular/directives/accordion.js
+++ js/angular/directives/accordion.js
@@ -4,19 +4,23 @@ angular.module('foundation.accordion')
   .controller('ZfAccordionController', ['$scope', function($scope) {
     var controller = this;
     var sections = controller.sections = $scope.sections = [];
-    var multiOpen = controller.multiOpen = false;
-    var autoOpen = controller.autoOpen = $scope.autoOpen = $scope.autoOpen || "true";
+    var multiOpen = controller.multiOpen = $scope.multiOpen = $scope.multiOpen || false;
+    var collapsible = controller.collapsible = $scope.collapsible = $scope.multiOpen || $scope.collapsible || true; //multi open infers a collapsible true
+    var autoOpen = controller.autoOpen = $scope.autoOpen = $scope.autoOpen || true; //auto open opens first tab on render
 
     controller.select = function(selectSection) {
       sections.forEach(function(section) {
+        //if multi open is allowed, toggle a tab
         if(controller.multiOpen) {
           if(section.scope === selectSection) {
             section.scope.active = !section.scope.active;
           }
         } else {
+          //non  multi open will close all tabs and open one
           section.scope.active = false;
           if(section.scope === selectSection) {
-            section.scope.active = true;
+            //if collapsible is allowed, a tab will toggle
+            section.scope.active = collapsible === true ? !section.scope.active : true;
           }
         }
 
@@ -26,7 +30,7 @@ angular.module('foundation.accordion')
     controller.addSection = function addsection(sectionScope) {
       sections.push({ scope: sectionScope });
 
-      if(sections.length === 1 && autoOpen === "true") {
+      if(sections.length === 1 && autoOpen === true) {
         sections[0].active = true;
         sections[0].scope.active = true;
       }
@@ -50,10 +54,13 @@ angular.module('foundation.accordion')
     controller: 'ZfAccordionController',
     scope: {
       multiOpen: '@?',
+      collapsible: '@?',
       autoOpen: '@?'
     },
     link: function(scope, element, attrs, controller) {
-      controller.multiOpen = scope.multiOpen === "true" ? true : false; //parse string into boolean
+      scope.multiOpen = controller.multiOpen = scope.multiOpen === "true" ? true : false;
+      scope.collapsible = controller.collapsible = scope.collapsible === "true" ? true : false;
+      scope.autoOpen = controller.autoOpen = scope.autoOpen === "true" ? true : false;
     }
   };
 });
