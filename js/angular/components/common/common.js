f4d7b655b798255de0bc32dafbb2e22867e27748
Jeanie Chung <jeanie.chung@gmail.com>
2014-12-17 12:05:07 -0600

    Added basic examples and documentation for swipe to close. Added basic directive for swipe to close integrating Hammer.JS recognizer.

--- js/angular/components/common/common.js
+++ js/angular/components/common/common.js
@@ -113,16 +113,32 @@
       var hammerElem;
       if (Hammer) {
         hammerElem = new Hammer(element[0]);
+        // set the options for swipe (to make them a bit more forgiving in detection)
+        hammerElem.get('swipe').set({
+          direction: Hammer.DIRECTION_ALL,
+          threshold: 5, // this is how far the swipe has to travel
+          velocity: 0.5 // and this is how fast the swipe must travel
+        });
       }
       // detect what direction the directive is pointing
-      if (attrs.zfSwipeClose === 'right') {
-        swipeDirection = 'panright'
-      }
-      else {
-        swipeDirection = 'panleft'
+      switch (attrs.zfSwipeClose) {
+        case 'right':
+          swipeDirection = 'swiperight';
+          break;
+        case 'left':
+          swipeDirection = 'swipeleft';
+          break;
+        case 'up':
+          swipeDirection = 'swipeup';
+          break;
+        case 'down':
+          swipeDirection = 'swipedown';
+          break;
+        default:
+          swipeDirection = 'swipe';
       }
       hammerElem.on(swipeDirection, function() {
-        foundationApi.closeActiveElements();
+        foundationApi.publish(attrs.id, 'close');
       });
     }
   }
