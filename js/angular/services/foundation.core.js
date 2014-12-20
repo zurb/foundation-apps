7ff67d07671e5e8faed09017ec3d64ac02d89fe5
MikaAK <mikakalathil@gmail.com>
2014-12-19 16:58:19 -0800

    Remove JSON.parse

--- js/angular/services/foundation.core.js
+++ js/angular/services/foundation.core.js
@@ -92,7 +92,26 @@
         return styleObject;
       }
 
-      styleObject = JSON.parse(str)
+      styleObject = str.split('&').reduce(function(ret, param) {
+        var parts = param.replace(/\+/g, ' ').split('=');
+        var key = parts[0];
+        var val = parts[1];
+        key = decodeURIComponent(key);
+
+        // missing `=` should be `null`:
+        // http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+        val = val === undefined ? null : decodeURIComponent(val);
+
+        if (!ret.hasOwnProperty(key)) {
+          ret[key] = val;
+        } else if (Array.isArray(ret[key])) {
+          ret[key].push(val);
+        } else {
+          ret[key] = [ret[key], val];
+        }
+        return ret;
+      }, {});
+
 
       return styleObject;
     }
