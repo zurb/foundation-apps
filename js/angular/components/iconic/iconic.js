927e8eb8f1b06484ee1801a786020df6c954310f
soumak77 <bsoumakian@gmail.com>
2015-02-12 15:53:22 -0800

    Updates for handling dynamic icon changes

--- js/angular/components/iconic/iconic.js
+++ js/angular/components/iconic/iconic.js
@@ -21,9 +21,9 @@
     }
   }
 
-  zfIconic.$inject = ['Iconic', 'FoundationApi', '$location']
+  zfIconic.$inject = ['Iconic', 'FoundationApi', '$compile', '$location']
 
-  function zfIconic(iconic, foundationApi, $location) {
+  function zfIconic(iconic, foundationApi, $compile, $location) {
     var directive = {
       restrict: 'A',
       template: '<img ng-transclude>',
@@ -31,6 +31,7 @@
       replace: true,
       scope: {
         dynSrc: '=?',
+        dynIcon: '=?',
         size: '@?',
         icon: '@'
       },
@@ -40,24 +41,26 @@
     return directive;
 
     function compile() {
+      var contents, assetPath = 'assets/img/iconic/';
+      
       return {
         pre: preLink,
         post: postLink
       };
 
       function preLink(scope, iElement, iAttrs, ctrl, transclude) {
-        var assetPath = 'assets/img/iconic/';
-
         if(scope.dynSrc) {
           iAttrs.$set('data-src', scope.dynSrc);
+        } else if(scope.dynIcon) {
+          iAttrs.$set('data-src', assetPath + scope.dynIcon + '.svg');
         } else {
-          // To support expressions on data-src
           if (scope.icon) {
             iAttrs.$set('data-src', assetPath + scope.icon + '.svg');
           } else {
+            // To support expressions on data-src
             iAttrs.$set('data-src', iAttrs.src);
           }
-        };
+        }
 
         var iconicClass;
         switch (scope.size) {
@@ -74,22 +77,59 @@
             iconicClass = 'iconic-fluid'
         }
         angular.element(iElement).addClass(iconicClass);
+        
+        // save contents of un-inject html, to use for dynamic re-injection
+        contents = iElement[0].outerHTML;
       }
 
       function postLink(scope, element, attrs) {
-        var ico = iconic.getAccess();
+        var svgElement, ico = iconic.getAccess();
 
-        ico.inject(element[0], {
-          each: function(injectedElem) {
-            if ($location.$$html5) {
-              scope.$emit('$zfIconicInjected', injectedElem);
-            }
-          }
-        });
+        injectSvg(element[0]);
 
         foundationApi.subscribe('resize', function() {
-          ico.update();
+          // only run update on current element
+          ico.update(element[0]);
         });
+        
+        // handle dynamic updating of icon
+        if(scope.dynSrc) {
+          scope.$watch('dynSrc', function(newVal, oldVal) {
+            if (newVal && newVal != oldVal) {
+              reinjectSvg(scope.dynSrc);
+            }
+          });
+        }
+        if (scope.dynIcon) {
+          scope.$watch('dynIcon', function(newVal, oldVal) {
+            if (newVal && newVal != oldVal) {
+              reinjectSvg(assetPath + scope.dynIcon + '.svg');
+            }
+          });
+        }
+        
+        function reinjectSvg(newSrc) {
+          if (svgElement) {
+            // set html
+            svgElement.empty();
+            svgElement.append(angular.element(contents));
+            
+            // set new source
+            svgElement.attr('data-src', newSrc);
+            
+            // reinject
+            injectSvg(svgElement[0]);
+          }
+        }
+
+        function injectSvg(element) {
+          ico.inject(element, {
+            each: function(injectedElem) {
+              var angElem = angular.element(injectedElem);
+              svgElement = $compile(angElem)(angElem.scope());
+            }
+          });
+        }
       }
     }
   }
