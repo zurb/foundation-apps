1cfa98b358a016380f98ed67d2e7732e0100ccb2
soumak77 <bsoumakian@gmail.com>
2015-03-20 03:42:14 -0700

    Implements feature zurb/foundation-apps#466

--- js/angular/components/interchange/interchange.js
+++ js/angular/components/interchange/interchange.js
@@ -123,18 +123,19 @@
   /*
    * This directive will configure ng-if and zf-if directives and then recompile
    */
-  function zfIfMediaWrapper(directiveName, mediaQuery, mediaOnly, mediaNot) {
+  function zfIfMediaWrapper(directiveName, mediaQuery, queryOnly, queryNot) {
     // set optional parameter
-    mediaOnly = mediaOnly || false;
-    mediaNot = mediaNot || false;
+    queryOnly = queryOnly || false;
+    queryNot = queryNot || false;
 
     zfIfMedia.$inject = [ '$compile', 'FoundationApi'];
     return zfIfMedia;
     
     function zfIfMedia($compile, foundationApi) {
-      // create unique scope property for media query result 
+      // create unique scope property for media query results
       // must be unique to avoid collision with other zf-if-* scopes
-      var mediaQueryProp = (directiveName + '_' + foundationApi.generateUuid()).replace(/-/g,'');
+      // property set upon element compilation
+      var mediaQueryProp;
       
       var directive = {
         priority: 1000, // must compile directive before any others
@@ -146,13 +147,17 @@
 
       return directive;
 
-      // from here onward, scope[mediaQueryProp] refers to the result of running the provided media query
+      // From here onward, scope[mediaQueryProp] refers to the result of running the provided media query
+      // against either the media or element as specified by the zf-if-type attribute.
       function compile(element, attrs) {
+        // assign unique name
+        mediaQueryProp = (directiveName + foundationApi.generateUuid()).replace(/-/g,'');
+        
         // set attributes to be read by zf-if directive
-        element.attr('zf-if-param-scope-prop', mediaQueryProp);
-        element.attr('zf-if-param-media', mediaQuery);
-        element.attr('zf-if-param-only', mediaOnly);
-        element.attr('zf-if-param-not', mediaNot);
+        element.attr('zf-if-scope-prop', mediaQueryProp);
+        element.attr('zf-if-query', mediaQuery);
+        element.attr('zf-if-only', queryOnly);
+        element.attr('zf-if-not', queryNot);
         
         // add zf-if directive
         element.attr('zf-if', 'zf-if');
@@ -183,29 +188,43 @@
   zfIfMediaFinal.$inject = [ '$compile', 'FoundationApi', 'FoundationMQ'];
   function zfIfMediaFinal($compile, foundationApi, foundationMQ) {
     var directive = {
-      priority: 601, // must compiled before ng-if (600)
+      priority: 601, // must compile before ng-if (600)
       restrict: 'A',
       compile: function compile(element, attrs) {
-        return compileWrapper(attrs['zfIfParamScopeProp'], attrs['zfIfParamMedia'], attrs['zfIfParamOnly'] === "true", attrs['zfIfParamNot'] === "true");
+        return compileWrapper(attrs['zfIfScopeProp'], attrs['zfIfQuery'], attrs['zfIfType'], attrs['zfIfOnly'] === "true", attrs['zfIfNot'] === "true");
       }
     };
 
     return directive;
 
-    // parameters will be populated with values provided from zf-if-param-* directive
-    function compileWrapper(mediaQueryProp, mediaQuery, mediaOnly, mediaNot) {
+    // parameters will be populated with values provided from zf-if-* attributes
+    function compileWrapper(mediaQueryProp, mediaQuery, queryType, queryOnly, queryNot) {
+      var isQuerySize = mediaQuery == "small" || mediaQuery == "medium" || mediaQuery == "large";
+      
+      // set defaults
+      queryType = queryType == "element" ? "element" : "media";
+      queryOnly = queryOnly || false;
+      queryNot = queryNot || false;
+      
+      // validations
+      if (!isQuerySize && queryType == "element") {
+        throw "Media query '" + mediaQuery + "' is supported with element queries. Only size queries (small/medium/large) are supported.";
+      }
+      
       return {
         pre: preLink,
         post: postLink
       }
-      
-      // from here onward, scope[mediaQueryProp] refers to the result of running the provided media query
+
+      // From here onward, scope[mediaQueryProp] refers to the result of running the provided media query
+      // against either the media or element as specified by the zf-if-type attribute.
       function preLink(scope, element, attrs) {
         // initially set media query result to false
         scope[mediaQueryProp] = false;
       }
 
       function postLink(scope, element, attrs) {
+        
         // subscribe for resize changes
         foundationApi.subscribe('resize', function(msg) {
           var orignalVisibilty = scope[mediaQueryProp];
@@ -220,13 +239,14 @@
         runQuery();
         
         function runQuery() {
-          var mediaUpQuery, mediaUpMinWidth, onlyMediaQuery;
+          var mediaUpQuery,
+              matchesQuery = (queryType === 'element' && isQuerySize) ? matchesElement : matchesMedia;
           
-          if (!mediaOnly) {
+          if (!queryOnly) {
             // run named query
-            scope[mediaQueryProp] = matchesMedia(mediaQuery);
+            scope[mediaQueryProp] = matchesQuery(mediaQuery);
           } else {
-            // run query with min-max range, by first getting media query string for next size up
+            // first get media query string for next size up
             switch (mediaQuery) {
             case "small":
               mediaUpQuery = "medium"; break;
@@ -236,46 +256,120 @@
               mediaUpQuery = null;  break;
             }
             
-            if (!mediaNot) {
+            if (!queryNot) {
+              /*
+               * Check that media ONLY matches named query and nothing else
+               */
               if (!mediaUpQuery) {
                 // reached max media size, run query for current media
-                scope[mediaQueryProp] = matchesMedia(mediaQuery);
+                scope[mediaQueryProp] = matchesQuery(mediaQuery);
               } else {
-                // extract min-width from next size up
-                mediaUpMinWidth = getMinWidthFromQuery(mediaUpQuery);
-                
-                // subtract 1/16 from the min width and use as max-width for current query
-                onlyMediaQuery = addMaxWidthToQuery(mediaQuery, mediaUpMinWidth - (1/16))
-                
-                // run query with min-width and max-width
-                scope[mediaQueryProp] = matchesMedia(onlyMediaQuery);
+                // must match this media size, but not the size up
+                scope[mediaQueryProp] = matchesQuery(mediaQuery) && !matchesQuery(mediaUpQuery);
               }
             } else {
+              /*
+               * Check that media does NOT match named query
+               */
               // require that the media doesn't match the current query
-              if (matchesMedia(mediaQuery)) {
-                // if it does match, make sure it also matches the next up media
-                scope[mediaQueryProp] = matchesMedia(mediaUpQuery)
-              } else {
+              if (!matchesQuery(mediaQuery)) {
                 // media doesn't match
                 scope[mediaQueryProp] = true;
+              } else {
+                // if it does match, make sure it also matches the next up media
+                if (!mediaUpQuery) {
+                  // no next size up, means media still matches
+                  scope[mediaQueryProp] = false;
+                } else {
+                  scope[mediaQueryProp] = matchesQuery(mediaUpQuery);
+                }
               }
             }
           }
         }
         
+        /** Source adapted from here: https://github.com/marcj/css-element-queries/blob/master/src/ElementQueries.js
+          * @copyright https://github.com/Mr0grog/element-query/blob/master/LICENSE
+          */
+        function getEmSize(element) {
+            var fontSize = element.prop('fontSize');
+            return parseFloat(fontSize) || 16;
+        }
+        
+        function convertToPx(value) {
+            var units = value.replace(/[0-9]*/, '');
+            value = parseFloat(value);
+            switch (units) {
+                case "px":
+                    return value;
+                case "em":
+                    // use directive element
+                    return value * getEmSize(element);
+                case "rem":
+                    // use document element
+                    return value * getEmSize(angular.element(document.documentElement));
+                // Viewport units!
+                // According to http://quirksmode.org/mobile/tableViewport.html
+                // documentElement.clientWidth/Height gets us the most reliable info
+                case "vw":
+                    return value * document.documentElement.clientWidth / 100;
+                case "vh":
+                    return value * document.documentElement.clientHeight / 100;
+                case "vmin":
+                case "vmax":
+                    var vw = document.documentElement.clientWidth / 100;
+                    var vh = document.documentElement.clientHeight / 100;
+                    var chooser = Math[units === "vmin" ? "min" : "max"];
+                    return value * chooser(vw, vh);
+                default:
+                    return value;
+                // for now, not supporting physical units (since they are just a set number of px)
+                // or ex/ch (getting accurate measurements is hard)
+            }
+        }
+        /**
+         * end @copyright https://github.com/Mr0grog/element-query/blob/master/LICENSE
+         */
+        
         function matchesMedia(query) {
           return foundationMQ.match([{media: query}]).length > 0;
         }
         
-        function getMinWidthFromQuery(query) {
-          var mediaString = foundationMQ.getMediaQueries()[query];
-          var matches = mediaString.match(/min-width: ([0-9]+)em/);
-          return matches[1];
+        function matchesElement(query) {
+          // use width of the element's parent for element query
+          // similar to how the root element is used for media queries
+          var parentElemWidth = element.parent().prop('offsetWidth');
+          
+          // extract min-width from query
+          var elemQueryMinWidth = getMinWidthInPxFromQuery(query);
+          
+          // check if element matches query
+          return parentElemWidth >= elemQueryMinWidth;
         }
         
-        function addMaxWidthToQuery(query, maxWidth) {
+        function getMinWidthInPxFromQuery(query) {
           var mediaString = foundationMQ.getMediaQueries()[query];
-          return mediaString + " and (max-width: " + maxWidth + "em)";
+          var matches = mediaString.match(/min-width: ([0-9]+)(em|px|rem|vw|vh|vmin|vmax|)/);
+          if (matches && matches.length == 3) {
+            return convertToPx(matches[1] + matches[2]);
+          } else {
+            return 0;
+          }
+        }
+        
+        function getMaxWidthInPxFromQuery(query) {
+          var mediaString = foundationMQ.getMediaQueries()[query];
+          var matches = mediaString.match(/max-width: ([0-9]+)(em|px|rem|vw|vh|vmin|vmax|)/);
+          if (matches && matches.length == 3) {
+            return convertToPx(matches[1] + matches[2]);
+          } else {
+            return 0;
+          }
+        }
+        
+        function addMaxWidthInPxToQuery(query, maxWidth) {
+          var mediaString = foundationMQ.getMediaQueries()[query];
+          return mediaString + " and (max-width: " + maxWidth + "px)";
         }
       }
     }
