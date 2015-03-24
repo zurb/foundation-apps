8f050a72ac7b54ac89658b531a3db37194abf5bf
soumak77 <bsoumakian@gmail.com>
2015-03-23 23:02:15 -0700

    Initial implementation of interchange directives

--- js/angular/components/interchange/interchange.js
+++ js/angular/components/interchange/interchange.js
@@ -91,4 +91,258 @@
     }
   }
 
+  angular.module('foundation.interchange')
+  /*
+   * Final directive to perform media/element queries, other directives set up this one
+   * (See: http://stackoverflow.com/questions/19224028/add-directives-from-directive-in-angularjs)
+   */
+    .directive('zfQuery', zfQuery)
+  /*
+   * zf-if-*
+   */
+    .directive('zfIfSmall', zfIf('small'))
+    .directive('zfIfMedium', zfIf('medium'))
+    .directive('zfIfLarge', zfIf('large'))
+    .directive('zfIfPortrait', zfIf('portrait'))
+    .directive('zfIfLandscape', zfIf('landscape'))
+    .directive('zfIfRetina', zfIf('retina'))
+    .directive('zfIfSmallOnly', zfIf('small', true))
+    .directive('zfIfMediumOnly', zfIf('medium', true))
+    .directive('zfIfLargeOnly', zfIf('large', true))
+    .directive('zfIfNotSmall', zfIf('small', true, true))
+    .directive('zfIfNotMedium', zfIf('medium', true, true))
+    .directive('zfIfNotLarge', zfIf('large', true, true))
+  /*
+   * zf-show-*
+   */
+    .directive('zfShowSmall', zfShow('small'))
+    .directive('zfShowMedium', zfShow('medium'))
+    .directive('zfShowLarge', zfShow('large'))
+    .directive('zfShowPortrait', zfShow('portrait'))
+    .directive('zfShowLandscape', zfShow('landscape'))
+    .directive('zfShowRetina', zfShow('retina'))
+    .directive('zfShowSmallOnly', zfShow('small', true))
+    .directive('zfShowMediumOnly', zfShow('medium', true))
+    .directive('zfShowLargeOnly', zfShow('large', true))
+    .directive('zfShowNotSmall', zfShow('small', true, true))
+    .directive('zfShowNotMedium', zfShow('medium', true, true))
+    .directive('zfShowNotLarge', zfShow('large', true, true))
+  /*
+   * zf-hide-*
+   */
+    .directive('zfHideSmall', zfHide('small'))
+    .directive('zfHideMedium', zfHide('medium'))
+    .directive('zfHideLarge', zfHide('large'))
+    .directive('zfHidePortrait', zfHide('portrait'))
+    .directive('zfHideLandscape', zfHide('landscape'))
+    .directive('zfHideRetina', zfHide('retina'))
+    .directive('zfHideSmallOnly', zfHide('small', true))
+    .directive('zfHideMediumOnly', zfHide('medium', true))
+    .directive('zfHideLargeOnly', zfHide('large', true))
+    .directive('zfHideNotSmall', zfHide('small', true, true))
+    .directive('zfHideNotMedium', zfHide('medium', true, true))
+    .directive('zfHideNotLarge', zfHide('large', true, true))
+  ;
+
+  function zfIf(namedQuery, queryOnly, queryNot) {
+    return zfQueryWrapper('ng-if', 'zf-if', namedQuery, queryOnly, queryNot);
+  }
+  function zfShow(namedQuery, queryOnly, queryNot) {
+    return zfQueryWrapper('ng-show', 'zf-show', namedQuery, queryOnly, queryNot);
+  }
+  function zfHide(namedQuery, queryOnly, queryNot) {
+    return zfQueryWrapper('ng-hide', 'zf-hide', namedQuery, queryOnly, queryNot);
+  }
+
+  /*
+   * This directive will configure ng-if/ng-show/ng-hide and zf-query directives and then recompile the element
+   */
+  function zfQueryWrapper(angularDirective, directiveName, namedQuery, queryOnly, queryNot) {
+    // set optional parameter
+    queryOnly = queryOnly || false;
+    queryNot = queryNot || false;
+
+    // generate directive name
+    if (queryNot) directiveName += '-not';
+    directiveName += '-' + namedQuery;
+    if (queryOnly && !queryNot) directiveName += '-only';
+
+    zfQueryDirective.$inject = [ '$compile', 'FoundationApi'];
+    return zfQueryDirective;
+
+    function zfQueryDirective($compile, foundationApi) {
+      // create unique scope property for media query result, must be unique to avoid collision with other properties of zf-query scope
+      // property set upon element compilation or else all similar directives (i.e. zf-if-*/zf-show-*/zf-hide-*) will get the same value
+      var queryResult;
+
+      var directive = {
+        priority: 1000, // must compile directive before any others
+        terminal: true, // don't compile any other directive after this
+                        // we'll fix this with a recompile
+        restrict: 'A',
+        compile: compile
+      };
+
+      return directive;
+
+      // From here onward, scope[queryResult] refers to the result of running the provided query
+      // against either the media or element as specified by the zf-query-type attribute.
+      function compile(element, attrs) {
+        var zfValue, ngValue;
+
+        // set unique property
+        queryResult = (directiveName + foundationApi.generateUuid()).replace(/-/g,'');
+
+        // add zf-query directive and configuration attributes
+        element.attr('zf-query', namedQuery);
+        element.attr('zf-query-not', queryNot);
+        element.attr('zf-query-only', queryOnly);
+        element.attr('zf-query-scope-prop', queryResult);
+
+        // check if value was given to directive attribute, if so pass to angular directive
+        zfValue = element.attr(directiveName) ||  element.attr('data-' + directiveName);
+        if (zfValue) {
+          ngValue = "(" + zfValue + " && " + queryResult + ")";
+        } else {
+          ngValue = queryResult;
+        }
+
+        // add/update angular directive
+        if (!element.attr(angularDirective)) {
+          element.attr(angularDirective, ngValue);
+        } else {
+          element.attr(angularDirective, ngValue + ' && (' + element.attr(angularDirective) + ')');
+        }
+
+        // remove current directive to avoid infinite recompile
+        element.removeAttr(directiveName);
+        element.removeAttr('data-' + directiveName);
+
+        return {
+          pre: function (scope, element, attrs) {
+          },
+          post: function (scope, element, attrs) {
+            // recompile
+            $compile(element)(scope);
+          }
+        }
+      }
+    }
+  }
+
+  zfQuery.$inject = [ '$compile', 'FoundationApi', 'FoundationMQ'];
+  function zfQuery($compile, foundationApi, foundationMQ) {
+    var directive = {
+      priority: 601, // must compile before ng-if (600)
+      restrict: 'A',
+      compile: function compile(element, attrs) {
+        return compileWrapper(attrs['zfQueryScopeProp'], attrs['zfQuery'], attrs['zfQueryType'], attrs['zfQueryOnly'] === "true", attrs['zfQueryNot'] === "true");
+      }
+    };
+
+    return directive;
+
+    // parameters will be populated with values provided from zf-if-* attributes
+    function compileWrapper(queryResult, namedQuery, queryType, queryOnly, queryNot) {
+      var isQuerySize = namedQuery == "small" || namedQuery == "medium" || namedQuery == "large";
+      var mediaUpMap = {
+        "small": "medium",
+        "medium": "large",
+        "large": null
+      };
+
+      // set defaults
+      queryType = queryType == "element" ? "element" : "media";
+      queryOnly = queryOnly || false;
+      queryNot = queryNot || false;
+
+      // validations
+      if (!isQuerySize && queryType == "element") {
+        throw "Named query '" + namedQuery + "' is supported with element queries. Only size queries (small/medium/large) are supported.";
+      }
+
+      return {
+        pre: preLink,
+        post: postLink
+      }
+
+      // From here onward, scope[queryResult] refers to the result of running the provided media query
+      // against either the media or element as specified by the zf-if-type attribute.
+      function preLink(scope, element, attrs) {
+        // initially set media query result to false
+        scope[queryResult] = false;
+      }
+
+      function postLink(scope, element, attrs) {
+        // subscribe for media cache cleared
+        foundationApi.subscribe('resize', function(time) {
+          var orignalVisibilty = scope[queryResult];
+          runQuery();
+          if (orignalVisibilty != scope[queryResult]) {
+            // digest if visibility changed
+            scope.$digest();
+          }
+        });
+
+        scope.$on("$destroy", function() {
+          foundationApi.unsubscribe('resize');
+        });
+
+        // run first media query check
+        runQuery();
+
+        function runQuery() {
+          var mediaUpNamedQuery,
+              matchesQuery = queryType === 'element' ? matchesElement : matchesMedia;
+
+          if (!queryOnly) {
+            // run named query
+            scope[queryResult] = matchesQuery(namedQuery);
+          } else {
+            mediaUpNamedQuery = mediaUpMap[namedQuery];
+
+            if (!queryNot) {
+              /*
+               * Check that media ONLY matches named query and nothing else
+               */
+              if (!mediaUpNamedQuery) {
+                // reached max media size, run query for current media
+                scope[queryResult] = matchesQuery(namedQuery);
+              } else {
+                // must match this media size, but not the size up
+                scope[queryResult] = matchesQuery(namedQuery) && !matchesQuery(mediaUpNamedQuery);
+              }
+            } else {
+              /*
+               * Check that media does NOT match named query
+               */
+              // require that the media doesn't match the current query
+              if (!matchesQuery(namedQuery)) {
+                // media doesn't match
+                scope[queryResult] = true;
+              } else {
+                // if it does match, make sure it also matches the next up media
+                if (!mediaUpNamedQuery) {
+                  // no next size up, means media still matches
+                  scope[queryResult] = false;
+                } else {
+                  scope[queryResult] = matchesQuery(mediaUpNamedQuery);
+                }
+              }
+            }
+          }
+        }
+
+        function matchesMedia(query) {
+          return foundationMQ.matchesMedia(query);
+        }
+
+        function matchesElement(query) {
+          // use width of the element's parent for element query
+          // similar to how the root element is used for media queries
+          return foundationMQ.matchesElement(element.parent(), query);
+        }
+      }
+    }
+  }
 })();
--- js/angular/services/foundation.mediaquery.js
+++ js/angular/services/foundation.mediaquery.js
@@ -129,10 +129,19 @@
   FoundationMQ.$inject = ['FoundationApi'];
 
   function FoundationMQ(foundationApi) {
-    var service = [];
+    var service = [],
+        mediaQueryResultCache = {},
+        elementQueryResultCache = {};
+
+    foundationApi.subscribe('resize', function() {
+      // any new resize event causes a clearing of the media cache
+      mediaQueryResultCache = {};
+    });
 
     service.getMediaQueries = getMediaQueries;
     service.match = match;
+    service.matchesMedia = matchesMedia;
+    service.matchesElement = matchesElement;
     service.collectScenariosFromElement = collectScenariosFromElement;
 
     return service;
@@ -166,6 +175,32 @@
       return matches;
     }
 
+    function matchesMedia(query) {
+      if (angular.isUndefined(mediaQueryResultCache[query])) {
+        // cache miss, run media query
+        mediaQueryResultCache[query] = match([{media: query}]).length > 0;
+      }
+      return mediaQueryResultCache[query];
+    }
+
+    function matchesElement(element, query) {
+      // get width of element
+      var elemWidth = element.prop('offsetWidth');
+
+      // check if we've run queries against this width before
+      if (angular.isUndefined(elementQueryResultCache[elemWidth])) {
+        // cache miss, setup cache for width
+        elementQueryResultCache[elemWidth] = {};
+      }
+
+      if (angular.isUndefined(elementQueryResultCache[elemWidth][query])) {
+        // cache miss, extract min-width from query and compare to element width
+        elementQueryResultCache[elemWidth][query] = elemWidth >= getMinWidthInPxFromQuery(query);
+      }
+
+      return elementQueryResultCache[elemWidth][query];
+    }
+
     // Collects a scenario object and templates from element
     function collectScenariosFromElement(parentElement) {
       var scenarios = [];
@@ -194,5 +229,59 @@
         templates: templates
       };
     }
+
+    /** Source adapted from here: https://github.com/marcj/css-element-queries/blob/master/src/ElementQueries.js
+     * @copyright https://github.com/Mr0grog/element-query/blob/master/LICENSE
+     */
+   function getEmSize(element) {
+       var fontSize = element.prop('fontSize');
+       return parseFloat(fontSize) || 16;
+   }
+
+   function convertToPx(element, value) {
+       var units = value.replace(/[0-9]*/, '');
+       value = parseFloat(value);
+       switch (units) {
+           case "px":
+               return value;
+           case "em":
+               // use directive element
+               return value * getEmSize(element);
+           case "rem":
+               // use document element
+               return value * getEmSize(angular.element(document.documentElement));
+           // Viewport units!
+           // According to http://quirksmode.org/mobile/tableViewport.html
+           // documentElement.clientWidth/Height gets us the most reliable info
+           case "vw":
+               return value * document.documentElement.clientWidth / 100;
+           case "vh":
+               return value * document.documentElement.clientHeight / 100;
+           case "vmin":
+           case "vmax":
+               var vw = document.documentElement.clientWidth / 100;
+               var vh = document.documentElement.clientHeight / 100;
+               var chooser = Math[units === "vmin" ? "min" : "max"];
+               return value * chooser(vw, vh);
+           default:
+               return value;
+           // for now, not supporting physical units (since they are just a set number of px)
+           // or ex/ch (getting accurate measurements is hard)
+       }
+   }
+   /**
+    * end @copyright https://github.com/Mr0grog/element-query/blob/master/LICENSE
+    */
+
+   function getMinWidthInPxFromQuery(query) {
+     var mediaString = getMediaQueries()[query];
+     var matches = mediaString.match(/min-width: ([0-9]+)(em|px|rem|vw|vh|vmin|vmax|)/);
+     if (matches && matches.length == 3) {
+       // use document element since query is media query against document width
+       return convertToPx(angular.element(document.documentElement), matches[1] + matches[2]);
+     } else {
+       return 0;
+     }
+   }
   }
 })();
